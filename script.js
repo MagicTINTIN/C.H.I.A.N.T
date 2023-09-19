5665737062efed2127be6efc007ad824cbf86cf9
MagicTINTIN <valentinservieres@gmail.com>
2023-09-19 18:40:17 +0300

    fully fonctional v1

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
+const INFO = {
+    startdate: "2023-08-28",
+    enddate: "2024-01-31",
+    eventname: "En Lituanie"
+}
+document.getElementById("dtitle").innerHTML = INFO.eventname.toUpperCase();
+
 let progressDiv = document.getElementById("progress");
 let progressValueDiv = document.getElementById("progressvalue");
 
-let progressValue = 14;
+const startDate = new Date(INFO.startdate);
+const endDate = new Date(INFO.enddate);
+const currentDate = new Date();
 
-progressValueDiv.textContent = `${progressValue}%`;
-progressDiv.style.background = `conic-gradient(
-      var(--active) ${progressValue * 3.6}deg,
-      var(--notactive) ${progressValue * 3.6}deg
+// Calculate days
+const currentToEnd = endDate - currentDate;
+const daysUntilEnd = Math.floor(currentToEnd / (1000 * 60 * 60 * 24));
+
+const startToCurrent = currentDate - startDate;
+const daysSinceStart = Math.floor(startToCurrent / (1000 * 60 * 60 * 24));
+
+const startToEnd = endDate - startDate;
+const daysTotal = Math.floor(startToEnd / (1000 * 60 * 60 * 24));
+
+// Calculate weeks
+const weeksUntilEnd = Math.floor(daysUntilEnd / 7);
+const weeksSinceStart = Math.floor(daysSinceStart / 7);
+const weeksTotal = Math.floor(daysTotal / 7);
+
+const progressValue = Math.floor(100 * daysSinceStart / daysTotal);
+document.getElementById("percentagesection").style.filter = `hue-rotate(${progressValue * 1.8}deg)`
+
+document.getElementById("ddaysleft").innerHTML = daysUntilEnd;
+document.getElementById("dday").innerHTML = daysSinceStart;
+document.getElementById("dtotaldays").innerHTML = daysTotal;
+
+document.getElementById("dweeksleft").innerHTML = weeksUntilEnd;
+document.getElementById("dweek").innerHTML = weeksSinceStart;
+document.getElementById("dtotalweeks").innerHTML = weeksTotal;
+
+let step=0, maxsteps = 70;
+let transi = setInterval(() => {
+    const progressTransi = step * progressValue / maxsteps;
+    progressValueDiv.textContent = `${Math.floor(progressTransi)}%`;
+    progressDiv.style.background = `conic-gradient(
+      var(--active) ${ progressTransi * 3.6}deg,
+      var(--notactive) ${progressTransi * 3.6}deg
   )`;
+    if (step >= maxsteps)
+        clearInterval(transi);
+    else
+        step++;
+}, 700 / maxsteps);
